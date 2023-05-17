4d655d002f950795b32fb9a6746f55653aecca58
OgutuJoseph <onyango.joseph@yahoo.com>
2023-05-17 15:10:40 +0300

    Add project files.

---
+++ angular-crud-api/Controllers/EmployeesController.cs
@@ -0,0 +1,38 @@
+﻿using angular_crud_api.Data;
+using angular_crud_api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace angular_crud_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly AngularCrudDbContext _angularCrudDbContext;
+
+        public EmployeesController(AngularCrudDbContext angularCrudDbContext)
+        {
+            _angularCrudDbContext = angularCrudDbContext;
+        }
+
+        [HttpGet]
+        public async Task <IActionResult> GetAllEmployees()
+        {
+            var employees = await _angularCrudDbContext.Employees.ToListAsync();
+
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> AddEmployee([FromBody] Employee employeeRequest)
+        {
+            employeeRequest.Id = Guid.NewGuid();
+
+            await _angularCrudDbContext.Employees.AddAsync(employeeRequest);
+            await _angularCrudDbContext.SaveChangesAsync();
+
+            return Ok(employeeRequest);
+        }
+    }
+}
---
+++ angular-crud-api/Data/AngularCrudDbContext.cs
@@ -0,0 +1,14 @@
+﻿using angular_crud_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace angular_crud_api.Data
+{
+    public class AngularCrudDbContext : DbContext
+    {
+        public AngularCrudDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
